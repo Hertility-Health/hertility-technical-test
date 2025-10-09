327b65f770fd65037ea14cf24b5c14e19a599643
Maxim London Gorshkov <gorshkov.maxim@gmail.com>
2025-10-09 09:57:07 +0100

    Update results.ts to fix TS syntax error

--- backend/src/services/results.ts
+++ backend/src/services/results.ts
@@ -13,7 +13,7 @@ interface Results {
 // this would normally be a database query - you don't need to change this function
 export async function fetchResults() {
     const json: { default: Results[] } = await import("../data/results.json", {
-        assert: { type: "json" },
+        with: { type: "json" },
     });
 	const results = json.default;
     return results;
