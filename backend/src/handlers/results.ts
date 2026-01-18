d8aa9e2a0c07718dbdbf7140610ec64764d6b818
hugo <hfarroba@googlemail.com>
2026-01-18 13:38:18 +0000

    chore: api: run npm audit + add backend dev tooling and configs

--- backend/src/handlers/results.ts
+++ backend/src/handlers/results.ts
@@ -2,7 +2,7 @@ import { Request, Response } from "express";
 import { fetchResults } from "../services/results";
 
 export const resultsHandler = async (_req: Request, res: Response) => {
-    const results = await fetchResults();
+  const results = await fetchResults();
 
-    res.send(results);
+  res.send(results);
 };
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,29 +1,29 @@
-import express from "express";
 import cors from "cors";
+import express from "express";
 import { resultsRouter } from "./routers/results";
 
 const PORT = 52863;
 const HOST = "0.0.0.0";
 
 function main() {
-    const app = express();
+  const app = express();
 
-    app.use(cors());
-    app.use(express.json());
+  app.use(cors());
+  app.use(express.json());
 
-    const router = express.Router();
+  const router = express.Router();
 
-    /* health check path */
-    router.get("/", (_req, res) => {
-        res.send("OK");
-    });
+  /* health check path */
+  router.get("/", (_req, res) => {
+    res.send("OK");
+  });
 
-   // api routers
-   app.use("/results", resultsRouter); 
+  // api routers
+  app.use("/results", resultsRouter);
 
-    app.listen(PORT, HOST, () => {
-        console.log(`Started API on ${HOST}:${PORT} 🚀 ✨`);
-    });
+  app.listen(PORT, HOST, () => {
+    console.log(`Started API on ${HOST}:${PORT} 🚀 ✨`);
+  });
 }
 
 main();
