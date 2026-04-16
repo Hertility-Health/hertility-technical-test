0efdc502f7a9fd839414258deac4405c7f4b28b5
Fatma Degirmenci <fatosdegirmenci644@gmilcom>
2026-04-17 00:54:48 +0100

    add hormone status calculation

--- backend/src/handlers/results.ts
+++ backend/src/handlers/results.ts
@@ -1,8 +1,17 @@
 import { Request, Response } from "express";
 import { fetchResults } from "../services/results";
+import { calculateStatus } from "../services/results";
 
 export const resultsHandler = async (_req: Request, res: Response) => {
-    const results = await fetchResults();
-
-    res.send(results);
+    try{
+        const results =await fetchResults();
+        const resultWithStatus=results.map((result)=>({
+            ...result,
+            status:calculateStatus(result),
+        }));
+        res.send(resultWithStatus);
+    }catch(error){
+        console.error(error);
+        res.status(500).send({message: "Something went wrong"});
+    }
 };
